cf080d291fd658bb5a9c06a22743a132acd7982c
Zikri adamm <tedon@example.com>
2026-09-09 11:40:53 +0800

    fix

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://tugas-aplikasi-kajian-lingkungan-hi.vercel.app/api';
 
 const getAuthHeaders = () => {
   const token = localStorage.getItem('token');
