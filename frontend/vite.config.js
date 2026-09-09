f2eb3bba49c06d5d9d0fccd4154fca9f8b3b3767
Zikri adamm <tedon@example.com>
2026-09-09 11:37:27 +0800

    fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://tugas-aplikasi-kajian-lingkungan-hi.vercel.app/',
         changeOrigin: true
       }
     }
